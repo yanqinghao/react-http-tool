5919c2a6bfdc2ee24903491a461570c7d24385cb
YanQinghao <woshiyanqinghao@gmail.com>
2020-09-16 10:13:28 +0800

    fix

--- web/components/RequestURL.tsx
+++ web/components/RequestURL.tsx
@@ -1,32 +1,25 @@
-import React, { Component } from 'react'
-import {
-    Form,
-    Select,
-    InputNumber,
-    DatePicker,
-    Switch,
-    Slider,
-    Button,
-} from 'antd'
-const FormItem = Form.Item
+import React, { Component } from "react";
+import { Form, InputNumber } from "antd";
+
+const FormItem = Form.Item;
 
 export default class RequestURL extends Component {
-    render() {
-        return (
-            <FormItem
-                label="Input Number"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 8 }}
-            >
-                <InputNumber
-                    size="large"
-                    min={1}
-                    max={10}
-                    style={{ width: 100 }}
-                    defaultValue={3}
-                    name="inputNumber"
-                />
-            </FormItem>
-        )
-    }
+  render() {
+    return (
+      <FormItem
+        label="Input Number"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <InputNumber
+          size="large"
+          min={1}
+          max={10}
+          style={{ width: 100 }}
+          defaultValue={3}
+          name="inputNumber"
+        />
+      </FormItem>
+    );
+  }
 }
--- web/components/RequstType.tsx
+++ web/components/RequstType.tsx
@@ -1,34 +1,18 @@
-import React, { Component } from 'react'
-import {
-    Form,
-    Select,
-    InputNumber,
-    DatePicker,
-    Switch,
-    Slider,
-    Button,
-} from 'antd'
+import React, { Component } from "react";
+import { Form, Select } from "antd";
 
-const FormItem = Form.Item
-const Option = Select.Option
+const FormItem = Form.Item;
+const Option = Select.Option;
 
 export default class RequstType extends Component {
-    render() {
-        return (
-            <FormItem
-                label="Select"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 8 }}
-            >
-                <Select
-                    size="large"
-                    defaultValue="get"
-                    style={{ width: 192 }}
-                >
-                    <Option value="get">GET</Option>
-                    <Option value="post">POST</Option>
-                </Select>
-            </FormItem>
-        )
-    }
+  render() {
+    return (
+      <FormItem label="Select" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+        <Select size="large" defaultValue="get" style={{ width: 192 }}>
+          <Option value="get">GET</Option>
+          <Option value="post">POST</Option>
+        </Select>
+      </FormItem>
+    );
+  }
 }
